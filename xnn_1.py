c8703e84e8286728e70ab9faf8df6ae86ce57295
xnn2018 <15914531973@139.com>
2019-02-22 16:10:56 +0800

    dkgkd

---
+++ xnn_1.py
@@ -0,0 +1,15 @@
+import unittest
+
+import xnn_test2
+
+
+class Test11(xnn_test2.Test22):
+    # def setUp(self):
+    #     print('Test11  setUp')
+    def m_test1(self):
+        print('m_test1')
+    # def test_2(self):
+    #     print('test_2')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ xnn_test2.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class Test22(unittest.TestCase):
+    def setUp(self):
+        print('Test22  setUp')
+    def test_44(self):
+        print('test_44')
+    def test_66(self):
+        print('test_66')
